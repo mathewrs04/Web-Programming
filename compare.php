2f3011f3488461f37b23306a093953a39377f401
Mathew Reynald <164150651+mathewrs04@users.noreply.github.com>
2025-08-24 18:03:43 +0700

    Add files via upload

---
+++ compare.php
@@ -0,0 +1,65 @@
+<?php
+$arrHape = array(
+    array("SKU"=>"12345", "Merk"=>"Samsung", "Model"=>"Galaxy S22 Plus", "Harga"=>18000000, "url_gambar"=>"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTffq5g6dXxBT2h-O2RWXgWp6jecWwguy_6jw&s", "spec"=>array("4GB", "5G", "5150mAH", "256GB")),
+    array("SKU"=>"54321", "Merk"=>"Xiaomi", "Model"=>"Mi 11 Pro", "Harga"=>9500000, "url_gambar"=>"https://images.tokopedia.net/img/cache/700/hDjmkQ/2024/3/22/3e120e54-94ab-4e18-87e3-bcbcb6d2b2b7.png", "spec"=>array("8GB", "5G", "5050mAH", "256GB")),
+    array("SKU"=>"AB012", "Merk"=>"Redmi", "Model"=>"Redmi Note 12", "Harga"=>6500000, "url_gambar"=>"https://cdn0-production-images-kly.akamaized.net/EiwZUMyVsfcemmM2b79W8W3dbnM=/800x450/smart/filters:quality(75):strip_icc():format(webp)/kly-media-production/medias/4208096/original/020202700_1667060581-Redmi_Note_12_Pro.jpg", "spec"=>array("6GB", "5G", "5350mAH", "256GB")),
+    array("SKU"=>"CD345", "Merk"=>"Oppo", "Model"=>"Reno 6", "Harga"=>7500000, "url_gambar"=>"https://www.static-src.com/wcsstore/Indraprastha/images/catalog/full//108/MTA-23983785/oppo_oppo_reno_6_full01_jlokiidu.jpg", "spec"=>array("6GB", "4G", "4500mAH", "128GB")),
+    array("SKU"=>"EF678", "Merk"=>"Pocophone", "Model"=>"X3 Pro", "Harga"=>4000000, "url_gambar"=>"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcROK_H-pOErtYGDmQn3T4Fys5YwVtepXeakew&s", "spec"=>array("6GB", "4G", "4000mAH", "128GB")),
+    array("SKU"=>"GH910", "Merk"=>"Realme", "Model"=>"GT Master", "Harga"=>5500000, "url_gambar"=>"https://asset-2.tstatic.net/tribunnews/foto/bank/images/realme-gt-master-edition-dan-realme-master-explorer.jpg", "spec"=>array("6GB", "5G", "4300mAH", "256GB")),
+    array("SKU"=>"IJ112", "Merk"=>"OnePlus", "Model"=>"9 Pro", "Harga"=>10000000, "url_gambar"=>"https://p-id.ipricegroup.com/media/Fanny/OnePlus_9_Pro__1_.jpg", "spec"=>array("12GB", "5G", "4500mAH", "256GB")),
+    array("SKU"=>"KL314", "Merk"=>"Apple", "Model"=>"iPhone 13", "Harga"=>10000000, "url_gambar"=>"https://images.tokopedia.net/img/cache/700/attachment/2021/10/8/115075297/115075297_805cee1c-1334-4adb-af53-ccad07d2ce81.jpg", "spec"=>array("4GB", "5G", "3095mAH", "128GB")),
+    array("SKU"=>"MN516", "Merk"=>"Google", "Model"=>"Pixel 6", "Harga"=>11000000, "url_gambar"=>"https://media.wired.com/photos/616e0b46436c4f5f5e47d166/191:100/w_1280,c_limit/Gear-Google-Pixel-6-top.jpg", "spec"=>array("8GB", "5G", "4600mAH", "128GB")),
+    array("SKU"=>"OP718", "Merk"=>"Samsung", "Model"=>"Z Flip 5", "Harga"=>13000000, "url_gambar"=>"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSprmI2RHN9_ix07piuM3adVxBBsTBTW259iQ&s", "spec"=>array("8GB", "5G", "4500mAH", "128GB")),
+    array("SKU"=>"QR920", "Merk"=>"Asus", "Model"=>"ROG Phone 5", "Harga"=>17000000, "url_gambar"=>"https://images.tokopedia.net/img/cache/700/attachment/2021/7/13/115075297/115075297_d45668d6-91b2-43bc-ac9f-6bd47ecd9fba.jpg", "spec"=>array("16GB", "5G", "6000mAH", "256GB")),
+    array("SKU"=>"ST122", "Merk"=>"Samsung", "Model"=>"Z Fold 5", "Harga"=>24000000, "url_gambar"=>"https://www.static-src.com/wcsstore/Indraprastha/images/catalog/full//catalog-image/MTA-116643906/samsung_samsung_galaxy_z_fold_5_5g_smartphone_12gb-512gb_full04_uo0degd2.jpg", "spec"=>array("12GB", "5G", "4400mAH", "512GB")),
+    array("SKU"=>"UV324", "Merk"=>"Huawei", "Model"=>"P50 Pro", "Harga"=>10000000, "url_gambar"=>"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcT-EE1uZ-ONYvrbTmXRB5F-bwxCBj4K62OAyw&s", "spec"=>array("8GB", "4G", "4360mAH", "256GB")),
+    array("SKU"=>"WX526", "Merk"=>"Apple", "Model"=>"Iphone 15", "Harga"=>14000000, "url_gambar"=>"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcT4wj4_6DrU683CblaCaD117gSa6Q71r8iNEA&s", "spec"=>array("6GB", "5G", "4000mAH", "256GB")),
+    array("SKU"=>"YZ728", "Merk"=>"Samsung", "Model"=>"Galaxy A05s", "Harga"=>2199000, "url_gambar"=>"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRwmWG8ovSbwts_f6m9vKDqW5Hh5u52WUztgQ&s", "spec"=>array("6GB", "5G", "5000mAH", "128GB")),
+    array("SKU"=>"AB930", "Merk"=>"Xiaomi", "Model"=>"Redmi Note 10 Pro", "Harga"=>3000000, "url_gambar"=>"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcS4Ot6qlHBP5MvTRFuTXk4MAdxtWilq21OpzQ&s", "spec"=>array("8GB", "5G", "5000mAH", "128GB")),
+    array("SKU"=>"CD132", "Merk"=>"Vivo", "Model"=>"V21 5G", "Harga"=>4000000, "url_gambar"=>"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSKSXw6eXoabJtCsMAXT0o17vI-p1hQlaMmUQ&s", "spec"=>array("8GB", "5G", "5000mAH", "128GB")),
+    array("SKU"=>"EF334", "Merk"=>"Samsung", "Model"=>"Galaxy A55", "Harga"=>6000000, "url_gambar"=>"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSMPvhKsscdXiD85m4pAhgAFxsxslgA1piDUg&s", "spec"=>array("8GB", "5G", "5000mAH", "256GB")),
+    array("SKU"=>"GH536", "Merk"=>"Realme", "Model"=>"Note 50", "Harga"=>1299000, "url_gambar"=>"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTj_Dq7U-6CEHrYRRpmEVr5tygaE-Ck2o9nkQ&s", "spec"=>array("4GB", "4G", "5000mAH", "128GB")),
+    array("SKU"=>"IJ738", "Merk"=>"Redmi", "Model"=>"Redmi 9T", "Harga"=>3000000, "url_gambar"=>"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTXn4EQNErpXBX0LOVE3Ys5yrk4Nfufoxy5DQ&s", "spec"=>array("6GB", "4G", "6000mAH", "128GB")),
+);
+
+$selected = isset($_GET['selected']) ? explode(",", $_GET['selected']) : [];
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+<meta charset="UTF-8">
+    <title>Perbandingan Hape</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+    <div class="sidebar">
+        <img src="https://i.pinimg.com/736x/f4/6a/5c/f46a5c8f95219d4d6823a747fb68dec8.jpg" alt="Gambar Logo" class="logo">
+            <h2>GabReyRam Cell</h2>
+        
+    </div>
+    <div class="main-content">
+        <div class="comparison-container">
+            <?php foreach ($selected as $index): ?>
+                <?php if (isset($arrHape[$index])): ?>
+                    <div class="comparison-card">
+                        <img src="<?= $arrHape[$index]['url_gambar'] ?>" alt="<?= $arrHape[$index]['Merk'] ?>">
+                        <h3><?= $arrHape[$index]['Merk'] ?></h3>
+                        <p><?= $arrHape[$index]['Model'] ?></p>
+                        <p>Rp <?= number_format($arrHape[$index]['Harga'], 0, ',', '.') ?></p>
+                        <p>Spesifikasi:</p>
+                        <ul class="spec-list">
+                            <?php foreach ($arrHape[$index]['spec'] as $spec): ?>
+                                <li><?= $spec ?></li>
+                            <?php endforeach; ?>
+                        </ul>
+                    </div>
+                <?php endif; ?>
+            <?php endforeach; ?>
+            
+        </div>
+        <br>
+        <a href="index.php">&lt;&lt; Kembali</a>
+    </div>
+</body>
+</html>
---
+++ index.php
@@ -0,0 +1,81 @@
+<?php
+$arrHape = array(
+    array("SKU"=>"12345", "Merk"=>"Samsung", "Model"=>"Galaxy S22 Plus", "Harga"=>18000000, "url_gambar"=>"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTffq5g6dXxBT2h-O2RWXgWp6jecWwguy_6jw&s", "spec"=>array("4GB", "5G", "5150mAH", "256GB")),
+    array("SKU"=>"54321", "Merk"=>"Xiaomi", "Model"=>"Mi 11 Pro", "Harga"=>9500000, "url_gambar"=>"https://images.tokopedia.net/img/cache/700/hDjmkQ/2024/3/22/3e120e54-94ab-4e18-87e3-bcbcb6d2b2b7.png", "spec"=>array("8GB", "5G", "5050mAH", "256GB")),
+    array("SKU"=>"AB012", "Merk"=>"Redmi", "Model"=>"Redmi Note 12", "Harga"=>6500000, "url_gambar"=>"https://cdn0-production-images-kly.akamaized.net/EiwZUMyVsfcemmM2b79W8W3dbnM=/800x450/smart/filters:quality(75):strip_icc():format(webp)/kly-media-production/medias/4208096/original/020202700_1667060581-Redmi_Note_12_Pro.jpg", "spec"=>array("6GB", "5G", "5350mAH", "256GB")),
+    array("SKU"=>"CD345", "Merk"=>"Oppo", "Model"=>"Reno 6", "Harga"=>7500000, "url_gambar"=>"https://www.static-src.com/wcsstore/Indraprastha/images/catalog/full//108/MTA-23983785/oppo_oppo_reno_6_full01_jlokiidu.jpg", "spec"=>array("6GB", "4G", "4500mAH", "128GB")),
+    array("SKU"=>"EF678", "Merk"=>"Pocophone", "Model"=>"X3 Pro", "Harga"=>4000000, "url_gambar"=>"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcROK_H-pOErtYGDmQn3T4Fys5YwVtepXeakew&s", "spec"=>array("6GB", "4G", "4000mAH", "128GB")),
+    array("SKU"=>"GH910", "Merk"=>"Realme", "Model"=>"GT Master", "Harga"=>5500000, "url_gambar"=>"https://asset-2.tstatic.net/tribunnews/foto/bank/images/realme-gt-master-edition-dan-realme-master-explorer.jpg", "spec"=>array("6GB", "5G", "4300mAH", "256GB")),
+    array("SKU"=>"IJ112", "Merk"=>"OnePlus", "Model"=>"9 Pro", "Harga"=>10000000, "url_gambar"=>"https://p-id.ipricegroup.com/media/Fanny/OnePlus_9_Pro__1_.jpg", "spec"=>array("12GB", "5G", "4500mAH", "256GB")),
+    array("SKU"=>"KL314", "Merk"=>"Apple", "Model"=>"iPhone 13", "Harga"=>10000000, "url_gambar"=>"https://images.tokopedia.net/img/cache/700/attachment/2021/10/8/115075297/115075297_805cee1c-1334-4adb-af53-ccad07d2ce81.jpg", "spec"=>array("4GB", "5G", "3095mAH", "128GB")),
+    array("SKU"=>"MN516", "Merk"=>"Google", "Model"=>"Pixel 6", "Harga"=>11000000, "url_gambar"=>"https://media.wired.com/photos/616e0b46436c4f5f5e47d166/191:100/w_1280,c_limit/Gear-Google-Pixel-6-top.jpg", "spec"=>array("8GB", "5G", "4600mAH", "128GB")),
+    array("SKU"=>"OP718", "Merk"=>"Samsung", "Model"=>"Z Flip 5", "Harga"=>13000000, "url_gambar"=>"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSprmI2RHN9_ix07piuM3adVxBBsTBTW259iQ&s", "spec"=>array("8GB", "5G", "4500mAH", "128GB")),
+    array("SKU"=>"QR920", "Merk"=>"Asus", "Model"=>"ROG Phone 5", "Harga"=>17000000, "url_gambar"=>"https://images.tokopedia.net/img/cache/700/attachment/2021/7/13/115075297/115075297_d45668d6-91b2-43bc-ac9f-6bd47ecd9fba.jpg", "spec"=>array("16GB", "5G", "6000mAH", "256GB")),
+    array("SKU"=>"ST122", "Merk"=>"Samsung", "Model"=>"Z Fold 5", "Harga"=>24000000, "url_gambar"=>"https://www.static-src.com/wcsstore/Indraprastha/images/catalog/full//catalog-image/MTA-116643906/samsung_samsung_galaxy_z_fold_5_5g_smartphone_12gb-512gb_full04_uo0degd2.jpg", "spec"=>array("12GB", "5G", "4400mAH", "512GB")),
+    array("SKU"=>"UV324", "Merk"=>"Huawei", "Model"=>"P50 Pro", "Harga"=>10000000, "url_gambar"=>"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcT-EE1uZ-ONYvrbTmXRB5F-bwxCBj4K62OAyw&s", "spec"=>array("8GB", "4G", "4360mAH", "256GB")),
+    array("SKU"=>"WX526", "Merk"=>"Apple", "Model"=>"Iphone 15", "Harga"=>14000000, "url_gambar"=>"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcT4wj4_6DrU683CblaCaD117gSa6Q71r8iNEA&s", "spec"=>array("6GB", "5G", "4000mAH", "256GB")),
+    array("SKU"=>"YZ728", "Merk"=>"Samsung", "Model"=>"Galaxy A05s", "Harga"=>2199000, "url_gambar"=>"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRwmWG8ovSbwts_f6m9vKDqW5Hh5u52WUztgQ&s", "spec"=>array("6GB", "5G", "5000mAH", "128GB")),
+    array("SKU"=>"AB930", "Merk"=>"Xiaomi", "Model"=>"Redmi Note 10 Pro", "Harga"=>3000000, "url_gambar"=>"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcS4Ot6qlHBP5MvTRFuTXk4MAdxtWilq21OpzQ&s", "spec"=>array("8GB", "5G", "5000mAH", "128GB")),
+    array("SKU"=>"CD132", "Merk"=>"Vivo", "Model"=>"V21 5G", "Harga"=>4000000, "url_gambar"=>"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSKSXw6eXoabJtCsMAXT0o17vI-p1hQlaMmUQ&s", "spec"=>array("8GB", "5G", "5000mAH", "128GB")),
+    array("SKU"=>"EF334", "Merk"=>"Samsung", "Model"=>"Galaxy A55", "Harga"=>6000000, "url_gambar"=>"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSMPvhKsscdXiD85m4pAhgAFxsxslgA1piDUg&s", "spec"=>array("8GB", "5G", "5000mAH", "256GB")),
+    array("SKU"=>"GH536", "Merk"=>"Realme", "Model"=>"Note 50", "Harga"=>1299000, "url_gambar"=>"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTj_Dq7U-6CEHrYRRpmEVr5tygaE-Ck2o9nkQ&s", "spec"=>array("4GB", "4G", "5000mAH", "128GB")),
+    array("SKU"=>"IJ738", "Merk"=>"Redmi", "Model"=>"Redmi 9T", "Harga"=>3000000, "url_gambar"=>"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTXn4EQNErpXBX0LOVE3Ys5yrk4Nfufoxy5DQ&s", "spec"=>array("6GB", "4G", "6000mAH", "128GB")),
+);
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>GabReyRam Cell</title>
+    <link rel="stylesheet" href="style.css">
+    <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
+    <script>
+        $(document).ready(function() {
+            $("input[type=checkbox]").change(function() {
+                var selected = $("input[type=checkbox]:checked").length;
+                if (selected === 3) {
+                    $("input[type=checkbox]").not(":checked").prop("disabled", true);
+                    $("#compareButton").prop("disabled", false);
+                } else {
+                    $("input[type=checkbox]").prop("disabled", false);
+                    $("#compareButton").prop("disabled", true);
+                }
+            });
+        });
+    </script>
+</head>
+<body>
+    <div class="sidebar">
+        <img src="https://i.pinimg.com/736x/f4/6a/5c/f46a5c8f95219d4d6823a747fb68dec8.jpg" alt="Gambar Logo" class="logo">
+        <h2>GabReyRam Cell</h2>
+    </div>
+    <div class="main-content">  
+        <div class="card-container">
+            <?php foreach ($arrHape as $index => $hape): ?>
+                <div class="card">
+                    <img src="<?= $hape['url_gambar'] ?>" alt="<?= $hape['Merk'] ?>">
+                    <h3><?= $hape['Merk'] ?></h3>
+                    <p><?= $hape['Model'] ?></p>
+                    <p>Rp <?= number_format($hape['Harga'], 0, ',', '.') ?></p>
+                    <input type="checkbox" name="compare[]" value="<?= $index ?>" id="checkbox<?= $index ?>">
+                    <label for="checkbox<?= $index ?>">Pilih</label>
+                </div>
+            <?php endforeach; ?>
+        </div>
+        <div class="button-container">
+            <button id="compareButton" disabled onclick="compareSelected()">Bandingkan</button>
+        </div>
+    </div>
+
+    <script>
+        function compareSelected() {
+            var selected = $("input[type=checkbox]:checked").map(function() {
+                return $(this).val();
+            }).get();
+            window.location.href = "compare.php?selected=" + selected.join(",");
+        }
+    </script>
+</body>
+</html>
+
